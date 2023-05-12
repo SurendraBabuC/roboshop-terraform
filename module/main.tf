6ec2f4c931b49289802c66bba875717f91aa1031
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-05-12 19:31:08 +0530

    Terraform

--- module/main.tf
+++ module/main.tf
@@ -28,8 +28,8 @@ resource "null_resource" "provisioner" {
 
 
 resource "aws_route53_record" "records" {
-  zone_id = "Z03986262CQPCHNJNZM9L"
-  name    = "${var.component_name}-dev.rdevopsb72.online"
+  zone_id = "Z06677293B4TGSKBEY0L9"
+  name    = "${var.component_name}-dev.surendrababuc01.online"
   type    = "A"
   ttl     = 30
   records = [aws_instance.instance.private_ip]
