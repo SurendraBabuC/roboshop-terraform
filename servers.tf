20fbd6b47ff7c91db3fd806a178045c02ced58de
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-05-12 19:21:58 +0530

    Terraform

--- servers.tf
+++ servers.tf
@@ -1,37 +1,24 @@
-resource "aws_instance" "instance" {
-  for_each               = var.components
-  ami                    = data.aws_ami.centos.image_id
-  instance_type          = each.value["instance_type"]
-  vpc_security_group_ids = [data.aws_security_group.allow-all.id]
+module "database-servers" {
+  for_each = var.database_servers
 
-  tags = {
-    Name = each.value["name"]
-  }
-
-  provisioner "remote-exec" {
-
-    connection {
-      type     = "ssh"
-      user     = "centos"
-      password = "DevOps321"
-      host     = self.private_ip
-    }
-
-    inline = [
-      "rm -rf roboshop-shell",
-      "git clone https://github.com/SurendraBabuC/roboshop-shell",
-      "cd roboshop-shell",
-      "sudo bash ${each.value["name"]}.sh"
-    ]
-  }
+  source         = "./module"
+  component_name = each.value["name"]
+  env            = var.env
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value, "password", "null")
+  provisioner    = true
+  app_type       = "db"
 }
 
+module "app-servers" {
+  depends_on = [module.database-servers]
+  for_each   = var.app_servers
 
-resource "aws_route53_record" "records" {
-  for_each = var.components
-  zone_id  = "Z06677293B4TGSKBEY0L9"
-  name     = "${each.value["name"]}-dev.surendrababuc01.online"
-  type     = "A"
-  ttl      = 30
-  records  = [aws_instance.instance[each.value["name"]].private_ip]
+  source         = "./module"
+  component_name = each.value["name"]
+  env            = var.env
+  instance_type  = each.value["instance_type"]
+  password       = lookup(each.value, "password", "null")
+  provisioner    = true
+  app_type       = "app"
 }
