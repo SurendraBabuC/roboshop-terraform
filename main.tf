d6c6bdf3540c916f320510eea1900d4364dabf67
C SURENDRA BABU <surendrababu.c01@gmail.com>
2023-06-05 21:12:55 +0530

    Ansible

--- main.tf
+++ main.tf
@@ -37,7 +37,7 @@ module "docdb" {
   source = "git::https://github.com/SurendraBabuC/tf-module-docdb.git"
 
   for_each       = var.docdb
-  subnet_ids     = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
+  subnets        = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["subnet_name"], null), "subnet_ids", null)
   allow_db_cidr  = lookup(lookup(lookup(lookup(module.vpc, "main", null), "subnets", null), each.value["allow_db_cidr"], null), "subnet_cidrs", null)
   engine_version = each.value["engine_version"]
   instance_count = each.value["instance_count"]
